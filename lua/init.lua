beff736932a12a772c3b8db3c03785f4ecb70a0d
Brandon Wong <brandon.wong@bitopiatech.com>
2022-09-26 20:11:18 +0800

    Add indent option in treesitter

--- lua/init.lua
+++ lua/init.lua
@@ -37,6 +37,7 @@ require 'nvim-treesitter.configs'.setup({
   ensure_installed = { "java" },
   sync_install = false,
   auto_install = true,
+  indent = { enable = true },
   
   highlight = {
     enable = true,
